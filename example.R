a12cef5c277daa91c493241400acea4840014c72
Sifan Liu <SLiu@brookings.edu>
2020-09-25 18:09:28 -0400

    finalize for sample lesson

---
+++ example.R
@@ -0,0 +1,10 @@
+df <- tibble::tribble(
+  ~state_code, ~state_name, ~county_code, ~county,
+  1L, "Alabama ", 1L, "Autauga County",
+  1L, "AL",       3L, "Baldwin county",
+  1L, " Alabama", 5L, "Barbour County",
+  1L, "Alabama ", 7L, "Bibb County",
+  1L, "AL",       9L, "Blount COUNTY",
+  1L, "Alabama", 11L, "Bullock County"
+)
+
